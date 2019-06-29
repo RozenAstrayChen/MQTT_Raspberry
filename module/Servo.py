fc96a4b2274ef474b6bdcddcad3a939272ad8822
Rozen <rozen@Rozende-MacBook-Pro.local>
2019-06-29 09:15:59 +0800

    fix bug

--- module/Servo.py
+++ module/Servo.py
@@ -2,12 +2,13 @@ import time
 import RPi.GPIO as GPIO
 
 # default
+'''
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 pwm = GPIO.PWM(18, 100)
 pwm.start(5)
-
+'''
 
 class Servo(object):
 	def __init__(self,pin):
@@ -15,7 +16,6 @@ class Servo(object):
 		self.GPIO.setmode(GPIO.BCM)
 		self.GPIO.setup(pin, GPIO.OUT)
 
-		self.pwm = pwm 
 		self.pwm = self.GPIO.PWM(pin, 100)
 		self.pwm.start(5)
 
